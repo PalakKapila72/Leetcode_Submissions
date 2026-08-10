1417e6ebd884701c79480fb3e017676da7667099
PalakKapila72 <24BCS10349@cuchd.in>
2026-08-10 15:34:34 +0530

    Time: 1 ms (53.66%), Space: 23.6 MB (74.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,16 +2,47 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
+        int candidate1;
+        int candidate2;
+        int count1=0;
+        int count2=0;
         vector<int> ans;
-
-        unordered_map<int,int> mp;
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        for(auto it:mp){
-            if(it.second>n/3){
-                ans.push_back(it.first);
+            if(nums[i]==candidate1){
+                count1++;
             }
+            else if(nums[i]==candidate2){
+                count2++;
+            }
+            else if(count1==0){
+                candidate1=nums[i];
+                count1=1;
+            }
+            else if(count2==0){
+                candidate2=nums[i];
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==candidate1){
+                count1++;
+            }
+            else if(nums[i]==candidate2){
+                count2++;
+
+            }
+        }
+        if(count1>n/3){
+            ans.push_back(candidate1);
+        }
+        if(candidate2!=candidate1 && count2>n/3){
+            ans.push_back(candidate2);
         }
         return ans;
     }
