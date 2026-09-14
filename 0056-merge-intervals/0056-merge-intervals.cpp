62002d8eb7af95ef4f627c6af12f384b9e349baa
PalakKapila72 <24BCS10349@cuchd.in>
2026-09-14 22:52:44 +0530

    Time: 9 ms (16.91%), Space: 23.9 MB (33.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
         int start=intervals[0][0];
         int end=intervals[0][1];
         for(int i=1;i<n;i++){
@@ -15,9 +15,9 @@ public:
                 start=intervals[i][0];
                 end=intervals[i][1];
             }
-
         }
         ans.push_back({start,end});
         return ans;
+
     }
 };
