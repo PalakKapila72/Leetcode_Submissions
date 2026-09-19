320eb9b00a1e3fbfa27d7acdfbe5f836897126a9
PalakKapila72 <24BCS10349@cuchd.in>
2026-09-19 12:49:54 +0530

    Time: 0 ms (100%), Space: 16.4 MB (70.02%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,42 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* dummy=new ListNode(0);
         dummy->next=head;
-        ListNode* prevgroup=dummy;
+        ListNode* groupPrev=dummy;
         while(true){
-            ListNode* kth=prevgroup;
+            ListNode* kth=groupPrev;
             for(int i=0;i<k;i++){
                 kth=kth->next;
-                if(kth==nullptr){
-                    return dummy->next;
-                }
+            
+            if(kth==nullptr){
+                return dummy->next;
             }
-            ListNode* nextgroup=kth->next;
-            ListNode* prev=nextgroup;
-            ListNode*curr=prevgroup->next;
-            while(curr!=nextgroup){
-                ListNode* next=curr->next;
-                curr->next=prev;
-                prev=curr;
-                curr=next;
+            
             }
-            ListNode* oldstart=prevgroup->next;
-            prevgroup->next=kth;
-            prevgroup=oldstart;
-
-
-        }
         
+        ListNode* groupNext=kth->next;
+        ListNode* prev=groupNext;
+        ListNode* curr=groupPrev->next;
+        while(curr!=groupNext){
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        ListNode* oldstart=groupPrev->next;
+        groupPrev->next=prev;
+        groupPrev=oldstart;
+        }
+
     }
 };
