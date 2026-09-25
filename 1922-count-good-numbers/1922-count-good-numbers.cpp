9ce68da38e32201ff6c00afe1971725ac5432bf5
PalakKapila72 <24BCS10349@cuchd.in>
2026-09-25 10:15:02 +0530

    Time: 0 ms (100%), Space: 8.1 MB (2.16%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long MOD=1e9+7;
+    long long power(long long x,long long n){
+        if(n==0){
+            return 1;
+        }
+        long long half=power(x,n/2);
+        if(n%2==0){
+            return half*half%MOD;
+        }
+        return ((x*half%MOD)*half%MOD);
+    }
+   
+    int countGoodNumbers(long long n) {
+        long long ans1=power(5,(n+1)/2);
+        long long ans2=power(4,n/2);
+        return ans1*ans2%MOD;
+    }
+};
